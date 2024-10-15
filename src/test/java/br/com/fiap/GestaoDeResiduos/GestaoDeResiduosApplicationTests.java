50752338a60e385b9f3409fc8744cf1a9f4be3fc
Guilherme <125518739+Guihermee@users.noreply.github.com>
2024-10-15 19:27:48 -0300

    CI

--- src/test/java/br/com/fiap/GestaoDeResiduos/GestaoDeResiduosApplicationTests.java
+++ src/test/java/br/com/fiap/GestaoDeResiduos/GestaoDeResiduosApplicationTests.java
@@ -3,11 +3,14 @@ package br.com.fiap.GestaoDeResiduos;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class GestaoDeResiduosApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void isEqual() {
+		assertEquals(4, 2 + 2);
 	}
 
 }
